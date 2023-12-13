7ce126169af453e285304931e623dbc1dd093495
Leila Zitouni <132995417+leilaZ1111@users.noreply.github.com>
2023-12-13 15:31:33 +0100

    added and commented alternative useHistory hook, alternative to the routerLink prop that we will keep for this project

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -8,8 +8,15 @@ import {
 } from '@ionic/react';
 
 import React from 'react';
+// import { useHistory } from 'react-router-dom'; // (ALTERNATIVE) we are importing the useHistory hook from the react-router-dom library. We will use this hook to set up the routing for our app.
 
 const Courses: React.FC = () => {
+  // const history = useHistory(); // (ALTERNATIVE) we are calling the useHistory hook. We will use this hook to set up the routing for our app.
+
+  // const changePageHandler = () => {
+  //   history.push('/course-goals'); // (ALTERNATIVE) we are calling the push() method on the history object. We will use this method to set up the routing for our app. We are passing the "/course-goals" path to the push() method. We are passing the "/course-goals" path to the push() method because we want to route to the /course-goals path, defined in App.tsx, when the button is clicked. //////////>>>>>>>>>> This is an alternative to using the routerLink prop that will work with the IonButton component and show as a "a" link when inspected in the browser.
+  // };
+
   return (
     <IonPage>
       <IonHeader>
@@ -21,6 +28,7 @@ const Courses: React.FC = () => {
         <h2> This works - Courses page!</h2>
         <div>
           <IonButton routerLink="/course-goals">To Course Goals</IonButton>
+          {/* <IonButton onClick={changePageHandler}>To Course Goals</IonButton> */}
         </div>
       </IonContent>
     </IonPage>
@@ -35,3 +43,5 @@ export default Courses;
 // (Now) The IonPage component is a component that we will use to wrap our app's routing. We will use the IonPage component to wrap the JSX code that we want to render. We will use the IonPage component to wrap the JSX code that we want to render because the IonPage component is a component that we will use to wrap our app's routing.
 
 // The routerLink prop is a prop that we can pass to the IonButton component. It's a string prop that we can set to the path that we want to route to. In this case, we want to set it to "/course-goals" because we want to route to the /course-goals path, defined in App.tsx, when the button is clicked.
+
+// (ALTERNATIVE)  <IonButton onClick={changePageHandler}>To Course Goals</IonButton>  is an alternative to using the routerLink prop. Unlike the routerLink prop, that will work with the IonButton component and show as a "a" link when inspected in the browser, the onClick prop will not show as a "a" link when inspected in the browser. Sometimes, we may want to use the onClick prop instead of the routerLink prop. For example, if we want to execute some JavaScript code when the button is clicked, then we may want to use the onClick prop instead of the routerLink prop.
